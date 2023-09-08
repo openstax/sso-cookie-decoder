8fe7f6b11081eb8d2014b506653155599af9a909
Nathan Stitt <nathan@stitt.org>
2023-09-08 13:57:42 -0500

    test using jsonwebtoken
    Turned out to be 151.4kb vs 68k for jose

--- src/decode.ts
+++ src/decode.ts
@@ -1,5 +1,7 @@
-import { compactDecrypt, compactVerify, importSPKI } from 'jose'
+//import { compactDecrypt, compactVerify, importSPKI } from 'jose'
 import { cookieConfig } from './config';
+import jwt from 'jsonwebtoken';
+import * as crypto from 'crypto';
 
 type Application = {
     id: number
@@ -35,15 +37,44 @@ export async function getUserFromCookies(
     const value = cookies[config.name]
     if (!value) return null
 
-    const { plaintext } = await compactDecrypt(value,
-        Buffer.from(config.private_key),
-        { contentEncryptionAlgorithms: ['A256GCM'], keyManagementAlgorithms: ['dir'] },
-    )
-    const { payload } = await compactVerify(
-        plaintext,
-        await importSPKI(config.public_key, 'RS256'),
-        { algorithms: ['RS256'] },
-    )
-    const jwt = JSON.parse(payload.toString()) as Token
-    return jwt.sub
+    const splitInput = value.split('.');
+    const iv = Buffer.from(splitInput[2], 'base64');
+    const decipher = crypto.createDecipheriv('aes-256-gcm', Buffer.from(config.private_key), iv, { authTagLength: 16 });
+
+    console.log(decipher)
+
+    const aad = new TextEncoder().encode(splitInput[0]);
+    const cipherText = Buffer.from(splitInput[3], 'base64');
+    const tag = Buffer.from(splitInput[4], 'base64');
+
+    decipher.setAAD(aad, { plaintextLength: cipherText.length });
+    decipher.setAuthTag(tag);
+
+    const result = Buffer.concat([
+        decipher.update(cipherText),
+        decipher.final(),
+    ]);
+
+    const plaintext = result.toString('utf-8')
+    console.log(plaintext)
+
+    const payload = jwt.verify(plaintext, config.public_key, {
+      clockTolerance: 300 // 5 minutes
+    });
+
+    console.log(payload)
+
+
+    // const { plaintext } = await compactDecrypt(value,
+    //     Buffer.from(config.private_key),
+    //     { contentEncryptionAlgorithms: ['A256GCM'], keyManagementAlgorithms: ['dir'] },
+    // )
+    // const { payload } = await compactVerify(
+    //     plaintext,
+    //     await importSPKI(config.public_key, 'RS256'),
+    //     { algorithms: ['RS256'] },
+    // )
+
+    //const val = JSON.parse(payload.toString()) as Token
+    return payload.sub
 }
